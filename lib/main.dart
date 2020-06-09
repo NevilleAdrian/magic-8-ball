103c8854cfa2cbb2d3af73ed9a9077a4c50386e1
nevilleadrian <nchukumah@infomall.ng>
2020-06-09 14:41:52 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask me anything',
+              style: TextStyle(
+                  fontSize: 30.0,
+              ),
+            ),
+            backgroundColor: Colors.blue.shade800,
+
+          ),
+          body: Decision(),
+        ),
       ),
     );
+
+class Decision extends StatefulWidget {
+  @override
+  _DecisionState createState() => _DecisionState();
+}
+
+class _DecisionState extends State<Decision> {
+  int ball = 1;
+
+  void rollBall () {
+    ball = Random().nextInt(4) + 1;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        onPressed: (){
+          setState(() {
+             rollBall();
+          });
+        },
+        child: Image.asset('images/ball$ball.png'),
+      ),
+    );
+  }
+}
